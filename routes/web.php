09b33992c51e0dcdaf886a47786776b89cfb3ac9
andriell <root.andriell@gmail.com>
2017-04-18 09:28:37 +0300

    start

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
-Route::get('/dir/{path}', 'DirController@index')
+Route::get('/dir/{path?}', 'DirController@index')
     ->where(['path' => '[\s\S]+']);
 
 Auth::routes();
