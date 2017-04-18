5ce653b22cf9505112f007c1bb72a238c341e8a5
andriell <root.andriell@gmail.com>
2017-04-18 09:06:56 +0300

    start

---
+++ app/Http/Controllers/DirController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 18.04.2017
+ * Time: 8:56
+ */
+
+namespace App\Http\Controllers;
+
+
+class DirController extends Controller
+{
+    function run() {
+        return 123;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dir/{path}', 'DirController@index')
+    ->where(['path' => '[\s\S]+']);
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
