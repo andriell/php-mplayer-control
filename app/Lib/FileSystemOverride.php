fb8850554fc35d9f94331563de645ffd9dbfafd1
andriell <root.andriell@gmail.com>
2017-04-19 17:33:43 +0300

    start

--- app/Lib/FileSystemOverride.php
+++ app/Lib/FileSystemOverride.php
@@ -25,62 +25,98 @@ class FileSystemOverride
 
     public function fileperms($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            fileperms($str);
+        }
         return fileperms(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function realpath($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            realpath($str);
+        }
         return iconv($this->fileSystemEncoding, 'UTF-8', realpath(iconv('UTF-8', $this->fileSystemEncoding, $str)));
     }
 
     public function is_dir($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            is_dir($str);
+        }
         return is_dir(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function filemtime($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            filemtime($str);
+        }
         return filemtime(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function filesize($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            filesize($str);
+        }
         return filesize(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function opendir($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            opendir($str);
+        }
         return opendir(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function readdir($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            readdir($str);
+        }
         $r = readdir($str);
         return is_bool($r) ? $r : iconv($this->fileSystemEncoding, 'UTF-8', $r);
     }
 
     public function imagecreatefromjpeg($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            imagecreatefromjpeg($str);
+        }
         return imagecreatefromjpeg(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function imagecreatefrompng($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            imagecreatefrompng($str);
+        }
         return imagecreatefrompng(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function imagecreatefromgif($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            imagecreatefromgif($str);
+        }
         return imagecreatefromgif(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function imagecreatefrombmp($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            imagecreatefrombmp($str);
+        }
         return imagecreatefrombmp(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 
     public function getimagesize($str)
     {
+        if (empty($this->fileSystemEncoding)) {
+            getimagesize($str);
+        }
         return getimagesize(iconv('UTF-8', $this->fileSystemEncoding, $str));
     }
 }
