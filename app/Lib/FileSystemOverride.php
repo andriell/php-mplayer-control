0f4f4c238f6cfb5e4328c0ac1f65ab7866e87b1a
andriell <root.andriell@gmail.com>
2017-04-24 22:27:25 +0300

    start

--- app/Lib/FileSystemOverride.php
+++ app/Lib/FileSystemOverride.php
@@ -25,10 +25,59 @@ class FileSystemOverride
 
     public function fileperms($str)
     {
-        if (empty($this->fileSystemEncoding)) {
-            return fileperms($str);
+        if (!empty($this->fileSystemEncoding)) {
+            $str = iconv('UTF-8', $this->fileSystemEncoding, $str);
         }
-        return fileperms(iconv('UTF-8', $this->fileSystemEncoding, $str));
+        $perms = fileperms($str);
+
+        switch ($perms & 0xF000) {
+            case 0xC000: // сокет
+                $info = 's';
+                break;
+            case 0xA000: // символическая ссылка
+                $info = 'l';
+                break;
+            case 0x8000: // обычный
+                $info = 'r';
+                break;
+            case 0x6000: // файл блочного устройства
+                $info = 'b';
+                break;
+            case 0x4000: // каталог
+                $info = 'd';
+                break;
+            case 0x2000: // файл символьного устройства
+                $info = 'c';
+                break;
+            case 0x1000: // FIFO канал
+                $info = 'p';
+                break;
+            default: // неизвестный
+                $info = 'u';
+        }
+        $info .= ': ';
+        // Владелец
+        $info .= (($perms & 0x0100) ? 'r' : '-');
+        $info .= (($perms & 0x0080) ? 'w' : '-');
+        $info .= (($perms & 0x0040) ?
+            (($perms & 0x0800) ? 's' : 'x' ) :
+            (($perms & 0x0800) ? 'S' : '-'));
+
+        // Группа
+        $info .= (($perms & 0x0020) ? 'r' : '-');
+        $info .= (($perms & 0x0010) ? 'w' : '-');
+        $info .= (($perms & 0x0008) ?
+            (($perms & 0x0400) ? 's' : 'x' ) :
+            (($perms & 0x0400) ? 'S' : '-'));
+
+        // Мир
+        $info .= (($perms & 0x0004) ? 'r' : '-');
+        $info .= (($perms & 0x0002) ? 'w' : '-');
+        $info .= (($perms & 0x0001) ?
+            (($perms & 0x0200) ? 't' : 'x' ) :
+            (($perms & 0x0200) ? 'T' : '-'));
+
+        return $info;
     }
 
     public function realpath($str)
