42cd5524c1e9091ea14b87162353d5f90c4a53ac
andriell <root.andriell@gmail.com>
2017-05-03 12:32:18 +0300

    Торрент

--- resources/views/torrent.blade.php
+++ resources/views/torrent.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Status</th>
-            <th>doneDate</th>
-            <th>haveValid</th>
-            <th>totalSize</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($torrents as $torrent)
+    <div class="panel panel-default">
+        <table class="table table-striped">
+            <thead>
             <tr>
-                <td>{{ $torrent['id'] }}</td>
-                <td>{{ $torrent['name'] }}</td>
-                <td>{{ $torrent['status'] }}</td>
-                <td>{{ $torrent['doneDate'] }}</td>
-                <td>{{ $torrent['haveValid'] }}</td>
-                <td>{{ $torrent['totalSize'] }}</td>
+                <th>Id</th>
+                <th>Название</th>
+                <th>Статус</th>
+                <th>Дата</th>
+                <th>haveValid</th>
+                <th>totalSize</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach ($torrents as $torrent)
+                <tr>
+                    <td>{{ $torrent['id'] }}</td>
+                    <td>{{ $torrent['name'] }}</td>
+                    <td>{{ $torrent['status'] }}</td>
+                    <td>{{ $torrent['doneDate'] }}</td>
+                    <td>{{ $torrent['haveValid'] }}</td>
+                    <td>{{ $torrent['totalSize'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
 
