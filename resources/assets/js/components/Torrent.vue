0f603b39deb7f9f5e4cd1597773499910fceb946
andriell <root.andriell@gmail.com>
2017-05-06 08:32:55 +0300

    Торрент

--- resources/assets/js/components/Torrent.vue
+++ resources/assets/js/components/Torrent.vue
@@ -20,84 +20,61 @@
             </table>
         </div>
         <div class="panel-body">
-
-
             <div class="list-group">
-                <div v-for="(item, itemId) in items" class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">{{ item.name }}</h5>
-                        <small class="text-muted">{{ f.date(item.addedDate) }}</small>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar" :style="f.widthP(item.percentDone, 1)">{{ f.percent(item.percentDone, 1) }}</div>
-                    </div>
+                <div v-for="(item, itemId) in items" class="list-group-item">
+                    <table class="torrent-table">
+                        <tr>
+                            <td rowspan="3" class="t-td-btm">
+                                <div class="dropdown">
+                                    <template v-if="item.status == 0">
+                                        <button class="btn btn-primary btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+                                            <span class="glyphicon glyphicon-pause"></span><br>{{f.torrentStatus(item.status)}}
+                                        </button>
+                                    </template>
+                                    <template v-else-if="item.status == 1 || item.status == 2 || item.status == 3">
+                                        <button class="btn btn-info btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+                                            {{item.status}}<span class="glyphicon glyphicon-time"></span><br>{{f.torrentStatus(item.status)}}
+                                        </button>
+                                    </template>
+                                    <template v-else-if="item.status == 4 || item.status == 5 || item.status == 6">
+                                        <button class="btn btn-success btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+                                            <span class="glyphicon glyphicon-play"></span><br>{{f.torrentStatus(item.status)}}
+                                        </button>
+                                    </template>
+                                    <template v-else="">
+                                        <button class="btn btn-default btn-lg dropdown-toggle" type="button" data-toggle="dropdown">
+                                            {{f.torrentStatus(item.status)}}
+                                        </button>
+                                    </template>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#" v-on:click="stop(item.id)"><span class="glyphicon glyphicon-pause"></span> Пауза</a></li>
+                                        <li><a href="#" v-on:click="start(item.id)"><span class="glyphicon glyphicon-play"></span> Запустить</a></li>
+                                        <li><a href="#" v-on:click="edit(item.id)"><span class="glyphicon glyphicon-edit"></span> Изменить</a></li>
+                                        <li><a href="#" v-on:click="remove(item.id)"><span class="glyphicon glyphicon-trash"></span> Удалить</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                            <td colspan="4" class="t-td-name"><h5>{{ item.name }}</h5></td>
+                            <td colspan="2" class="t-td-date"><small class="text-muted">{{ f.date(item.addedDate) }}</small></td>
+                        </tr>
+                        <tr>
+                            <td colspan="6" class="t-td-progress">
+                                <div class="progress">
+                                    <div class="progress-bar" :style="f.widthP(item.percentDone, 1)">{{ f.percent(item.percentDone, 1) }}</div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="t-td-size">{{ f.size(item.sizeWhenDone) }}</td>
+                            <td class="t-td-eta">00:00:00{{ f.seconds(item.eta) }}</td>
+                            <td class="t-td-leechers">{{ item.webseedsSendingToUs }}</td>
+                            <td class="t-td-upload">{{ f.speed(item.rateUpload) }}</td>
+                            <td class="t-td-seeders">{{ item.peersSendingToUs }}</td>
+                            <td class="t-td-download">{{ f.speed(item.rateDownload) }}</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
-
-
-            <table class="table table-striped table-data">
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Название</th>
-                    <th style="width: 100px">Размер</th>
-                    <th>Готово</th>
-                    <th>Статус</th>
-                    <th>Сиды</th>
-                    <th style="width: 100px">Загрузка</th>
-                    <th style="width: 100px">Отдача</th>
-                    <th>Осталось</th>
-                    <th>Дата</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(item, itemId) in items">
-                    <td>{{ item.id }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ f.size(item.sizeWhenDone) }}</td>
-                    <td>
-                        <div class="progress">
-                            <div class="progress-bar" :style="f.widthP(item.percentDone, 1)">{{ f.percent(item.percentDone, 1) }}</div>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="dropdown">
-                            <template v-if="item.status == 0">
-                                <button class="btn btn-primary btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
-                                    <span class="glyphicon glyphicon-pause"></span> {{f.torrentStatus(item.status)}} <span class="caret"></span>
-                                </button>
-                            </template>
-                            <template v-else-if="item.status == 1 || item.status == 2 || item.status == 3">
-                                <button class="btn btn-info btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
-                                    {{item.status}}<span class="glyphicon glyphicon-time"></span> {{f.torrentStatus(item.status)}} <span class="caret"></span>
-                                </button>
-                            </template>
-                            <template v-else-if="item.status == 4 || item.status == 5 || item.status == 6">
-                                <button class="btn btn-success btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
-                                    <span class="glyphicon glyphicon-play"></span> {{f.torrentStatus(item.status)}} <span class="caret"></span>
-                                </button>
-                            </template>
-                            <template v-else="">
-                                <button class="btn btn-default btn-xs dropdown-toggle" type="button" data-toggle="dropdown">
-                                    {{f.torrentStatus(item.status)}} <span class="caret"></span>
-                                </button>
-                            </template>
-                            <ul class="dropdown-menu">
-                                <li><a href="#" v-on:click="stop(item.id)"><span class="glyphicon glyphicon-pause"></span> Пауза</a></li>
-                                <li><a href="#" v-on:click="start(item.id)"><span class="glyphicon glyphicon-play"></span> Запустить</a></li>
-                                <li><a href="#" v-on:click="edit(item.id)"><span class="glyphicon glyphicon-edit"></span> Изменить</a></li>
-                                <li><a href="#" v-on:click="remove(item.id)"><span class="glyphicon glyphicon-trash"></span> Удалить</a></li>
-                            </ul>
-                        </div>
-                    </td>
-                    <td>{{ item.peersSendingToUs }}</td>
-                    <td>{{ f.speed(item.rateDownload) }}</td>
-                    <td>{{ f.speed(item.rateUpload) }}</td>
-                    <td>{{ f.seconds(item.eta) }}</td>
-                    <td>{{ f.date(item.addedDate) }}</td>
-                </tr>
-                </tbody>
-            </table>
         </div>
     </div>
 </template>
@@ -189,12 +166,6 @@
 </script>
 
 <style>
-    .torrent .d-flex {
-        justify-content: space-between;
-        display: -webkit-box!important;
-        display: -ms-flexbox!important;
-        display: flex!important;
-    }
     .torrent .progress .progress-bar {
         font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
         line-height: 12px;
@@ -202,4 +173,65 @@
     .torrent .progress {
         height: 12px;
     }
+    .torrent .torrent-table {
+        width: 100%;
+        height: 72px;
+    }
+    .torrent .torrent-table td {
+        /*border: 1px solid black;*/
+    }
+    .torrent .torrent-table .t-td-btm {
+        width: 72px;
+        padding-right: 10px;
+    }
+    .torrent .torrent-table .t-td-btm .btn {
+        font-size: 10px;
+        width: 72px;
+        height: 72px;
+        padding: 2px;
+        text-align: center;
+    }
+    .torrent .torrent-table .t-td-btm .btn .glyphicon {
+        font-size: 30px;
+    }
+
+    .torrent .torrent-table .t-td-eta {
+        text-align: center;
+    }
+
+    .torrent .torrent-table .t-td-name {
+        max-width: 100px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
+    .torrent .torrent-table .t-td-date {
+        vertical-align: top;
+    }
+
+    .torrent .torrent-table .t-td-date,
+    .torrent .torrent-table .t-td-size,
+    .torrent .torrent-table .t-td-leechers,
+    .torrent .torrent-table .t-td-upload,
+    .torrent .torrent-table .t-td-seeders,
+    .torrent .torrent-table .t-td-download {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+
+    .torrent .torrent-table .t-td-size,
+    .torrent .torrent-table .t-td-leechers,
+    .torrent .torrent-table .t-td-upload,
+    .torrent .torrent-table .t-td-seeders,
+    .torrent .torrent-table .t-td-download {
+        padding: 0 5px;
+        width: 72px;
+    }
+    .torrent .torrent-table .t-td-date,
+    .torrent .torrent-table .t-td-leechers,
+    .torrent .torrent-table .t-td-seeders {
+        text-align: right;
+    }
+
+
 </style>
