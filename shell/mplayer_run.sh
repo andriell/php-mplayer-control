148612493f400cb6f8bb2403f4c041762d9be7d2
andriell <root.andriell@gmail.com>
2017-10-13 19:08:34 +0300

    Субтитры fix

--- shell/mplayer_run.sh
+++ shell/mplayer_run.sh
@@ -4,5 +4,5 @@ killall mplayer
 rm -rf "$3"
 rm -rf "$2"
 mkfifo "$2" -m 0644
-export DISPLAY=:0.0 && mplayer -ao alsa:device=hw=0.3 -quiet -fs -slave -input file="$2" "$1" > "$3" 2> /dev/null &
+export DISPLAY=:0.0 && mplayer -utf8 -ao alsa:device=hw=0.3 -quiet -fs -slave -input file="$2" "$1" > "$3" 2> /dev/null &
 chmod 0644 "$3"
