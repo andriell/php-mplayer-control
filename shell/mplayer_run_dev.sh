dc461db96f7180e40318a6b05dd4060d288e8ac6
andriell <root.andriell@gmail.com>
2019-01-04 20:39:30 +0300

    fix

--- shell/mplayer_run_dev.sh
+++ shell/mplayer_run_dev.sh
@@ -4,5 +4,5 @@ killall mplayer
 rm -rf "$3"
 rm -rf "$2"
 mkfifo "$2" -m 0644
-export DISPLAY=:0.0 && mplayer -utf8 -quiet -fs -slave -input file="$2" "$1" > "$3" 2> /dev/null &
+export DISPLAY=:0.0 && mplayer -vo gl -utf8 -quiet -fs -slave -input file="$2" "$1" > "$3" 2> /dev/null &
 chmod 0644 "$3"
